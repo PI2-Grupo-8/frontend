d01192b96a6c9ee5a9018677f73534b388e9cee4
Gabriela Guedes <gabrielabguedes@gmail.com>
2021-10-01 15:31:57 -0300

    Add layout to list of vehicles
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- src/components/VehicleCard/index.js
+++ src/components/VehicleCard/index.js
@@ -1,24 +1,59 @@
 import * as React from 'react';
+import moment from 'moment'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { CardActionArea } from '@mui/material';
+import { purple, white, green, yellow } from '../../constants/colors';
+import Battery from '../Battery';
+import 'moment/locale/pt-br'
+import './style.css'
+
+import { BsFillCircleFill } from "react-icons/bs";
+import { RiOilFill } from "react-icons/ri";
+import { GoAlert } from "react-icons/go";
+
 
 const VehicleCard = ({ vehicle }) =>  {
+  moment.locale("pt-br");
+
+  // TODO: Get alets by vehicle
+  // Definições: Ultima atualização é o tempo do ultimo dado de sensor enviado?
+  // Como saberemos se o trator está em funcionamento no momento?
+
+
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ width: 350, margin: 1, background: purple, color: white }}>
       <CardActionArea>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {vehicle.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <div className="card-header">
+            <div className="card-title">
+              <h2 className="vehicle-name">
+                {vehicle.name}
+              </h2>
+              <BsFillCircleFill color={green}/>
+            </div>
+            <div className="data-icons">
+              <div className="info">
+                <p className="percentage">75%</p>
+                <Battery percentage={75} color={green} />
+              </div>
+              <div className="info">
+                <p className="percentage">100%</p>
+                <RiOilFill color={green} size={20} />
+              </div>
+            </div>
+          </div>
+          <Typography paragraph color={white}>
             {vehicle.description}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {vehicle.updatedAt}
-          </Typography>
+          <div className="card-footer">
+            <Typography variant="subtitle" color={white}>
+              Ultima atualização: {moment(vehicle.updatedAt).fromNow()}
+            </Typography>
+            <GoAlert color={yellow} size={20}/>
+          </div>
         </CardContent>
       </CardActionArea>
     </Card>
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,6 +4,10 @@ import { useAlertContext } from '../../contexts/alertsContext';
 import VehicleCard from '../../components/VehicleCard'
 import { getVehiclesByUser } from '../../services/axios/vehicleService';
 import './style.css'
+import Fab from '@mui/material/Fab';
+import { MdAdd } from "react-icons/md";
+import Box from '@mui/material/Box';
+import { green, white } from '../../constants/colors';
 
 const HomePage = () => {
   const [vehicles, setVehicles] = useState([])
@@ -26,9 +30,23 @@ const HomePage = () => {
     <div className="page-container">
       <h1 className='title'>Meus Veículos</h1>
       <div className="cards">
-        {vehicles.map((vehicle) =>
-          <VehicleCard vehicle={vehicle} />
+        {vehicles.map((vehicle, idx) =>
+          <VehicleCard vehicle={vehicle} key={idx} />
         )}
+      </div>
+      <div className="add-button">
+
+      <Fab sx={{
+          background: green,
+          color: white,
+          '&:hover': {
+            backgroundColor: green
+          }
+        }}
+        onClick={() => history.push('/vehicle/create')}
+        >
+        <MdAdd size={25}/>
+      </Fab>
       </div>
     </div>
   );
