f26db13b53b18efe7ea358f0aef8f4d459255f86
Gabriela Guedes <gabrielabguedes@gmail.com>
2021-10-18 23:31:18 -0300

    Connect alerts page
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- src/pages/Alerts/index.js
+++ src/pages/Alerts/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
-
+import { closeAlert } from '../../services/axios/sensorsDataService';
 import { useAlertContext } from '../../contexts/alertsContext';
 import { getAlerts } from '../../services/axios/sensorsDataService';
 import ActiveAlert from '../../components/Alerts/ActiveAlert';
@@ -8,45 +8,57 @@ import InactiveAlert from '../../components/Alerts/InactiveAlert';
 import './style.css'
 
 const Alerts = () => {
-    const [alerts, setAlerts] = useState({});
-    const { showErrorAlert } = useAlertContext()
+  const [alerts, setAlerts] = useState({});
+  const { showErrorAlert, showSuccessAlert } = useAlertContext()
 
-    const getAllAlerts = async () => {
-        const req = await getAlerts();
-        if (req.success) {
-            console.log(req.data)
-            setAlerts(req.data);
-        } else {
-            showErrorAlert('Ocorreu um erro ao buscar alertas')
-        }
+  const getAllAlerts = async () => {
+    const req = await getAlerts();
+    if (req.success) {
+      console.log(req.data)
+      setAlerts(req.data);
+    } else {
+      showErrorAlert('Ocorreu um erro ao buscar alertas')
     }
+  }
 
-    useEffect(() => {
-        getAllAlerts();
-    }, []);
+  const closeActiveAlert = async (id) => {
+    const request = await closeAlert(id)
+    if (request.success) {
+      showSuccessAlert('Alerta finalizado!')
+      getAllAlerts()
+      return
+    }
+    showErrorAlert('Ocorreu um erro ao finalizar alerta')
+  }
 
-    return (
-        <div>
-            {window.screen.width < 800 ?
-                <>
-                    <h1 className="alert-container-title">Lista de alertas</h1>
-                    <div className="alert-list">
-                        {
-                            alerts?.opened?.map(alert => <ActiveAlert alert={alert} key={alert._id} />)
-                        }
-                        <div className="alert-separator" />
-                        <h1 className="alert-history-title" >Histórico de Alertas</h1>
-                        {
-                            alerts?.closed?.map(alert => <InactiveAlert alert={alert} key={alert._id} />)
-                        }
-                    </div>
-                </>
-                : <Redirect
-                    to={{ pathname: '/' }}
-                />
+  useEffect(() => {
+    getAllAlerts();
+  }, []);
+
+  return (
+    <div>
+      {window.screen.width < 800 ?
+        <>
+          <h1 className="alert-container-title">Lista de alertas</h1>
+          <div className="alert-list">
+            {
+              alerts?.opened?.map(alert =>
+                <ActiveAlert alert={alert} key={alert._id} onClick={() => closeActiveAlert(alert._id)}/>
+              )
             }
-        </div>
-    );
+            <div className="alert-separator" />
+            <h1 className="alert-history-title" >Histórico de Alertas</h1>
+            {
+              alerts?.closed?.map(alert => <InactiveAlert alert={alert} key={alert._id} />)
+            }
+          </div>
+        </>
+        : <Redirect
+          to={{ pathname: '/' }}
+        />
+      }
+    </div>
+  );
 }
 
 export default Alerts;
